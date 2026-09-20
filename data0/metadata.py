6be75ba6f55fb862828eba414e0c50cde0afdb97
DeadneM <deadnem@gmail.com>
2026-09-20 11:29:16 +0200

    Add compact deterministic Data0 metadata

---
+++ data0/metadata.py
@@ -0,0 +1,29 @@
+# Build 15 Data0 metadata. Only values differing per entry are stored.
+BASIC = {
+    'data/settings/defaultvideoquality.scr': (6, 'fd6e820c745d7d1983d04acb', 22929, 17063, 3412905228, 620, 1412),
+    'data/menu/scr/uigamemoviebackground.xui': (9, '53c4e1568a7437c4b4eff21e', 19055, 17089, 516152720, 7362, 212307),
+    'data/menu/scr/menuvariablescheat.xui': (6, '52ce97bb3cefcb899f9a1260', 47565, 17046, 1621656780, 1063, 4547),
+    'data/menu/scr/menustartlevel.xui': (6, '2444f0f719edce5cb970ee26', 47565, 17046, 647778223, 710, 3009),
+    'data/menu/scr/menuskin.xui': (6, '2f63a47acd7aa0f86308420d', 47475, 17066, 232441602, 39936, 766553),
+    'data/menu/scr/menuprogresscheat.xui': (6, 'af8dd79e166e5216bc47cf41', 47565, 17046, 1100344028, 1107, 4676),
+    'data/menu/scr/menuinvokequickcheat.xui': (6, '781123d5c9f999d18e933f7b', 47565, 17046, 2064058315, 1116, 5023),
+    'data/menu/scr/menuinvokequeststate.xui': (6, '68e1f700b2241bcba4556891', 47565, 17046, 2445863841, 1082, 4865),
+    'data/menu/scr/menuinvokecheat.xui': (6, '14626bb1a334860664884026', 47565, 17046, 653110394, 1070, 4811),
+    'data/menu/scr/menudeveloperlist.xui': (6, '6c1d21fac1b3b5255af17d16', 47565, 17046, 377367211, 1003, 4561),
+    'data/menu/scr/menudeveloperitems.xui': (6, '216dcf79fb1027bfbae8ac06', 47565, 17046, 109345464, 1026, 4268),
+    'data/menu/scr/menucollectablecheat.xui': (6, 'b7d1fad1fc12c81aab7706ed', 47565, 17046, 3979457297, 613, 2496),
+    'data/menu/scr/menucheat.xui': (6, '9f52a9374d92b5618b9d820a', 47565, 17046, 181726843, 1126, 4946),
+    'data/menu/scr/menuaidebug.xui': (6, '73ff96cfc9326531b35ace71', 47565, 17046, 1896696469, 786, 3446),
+    'data/menu/hud/hud.xui': (6, '4734af8b8234f2e22c59f425', 19055, 17089, 624576213, 49918, 805792),
+    'data/inventory.scr': (6, 'cabacea0ce863c5643c7e24c', 19682, 23854, 2405075281, 5651, 100728),
+    'data/inputs_keyboard.scr': (6, 'd64142d5021fe8410a71a49d', 40356, 23854, 3394484824, 2227, 15782),
+    'data/inputs_def.scr': (6, '70a797e728a7c19f30c7dd83', 33757, 23854, 2791315983, 4111, 19215),
+    'data/menu/movies/intromovies.scr': (6, 'ecc3183b71d1d7b79c703d3e', 47564, 17046, 1052260489, 427, 899),
+}
+
+SPECIAL = {
+    'data/settings/defaultvideoquality.scr': {'flag': 0, 'comment': 'd8fb9d'},
+    'data/inventory.scr': {'flag': 9, 'vm': 798, 'exta': 2175008768, 'local_extra': '5554090003b7c0a76ab7c0a76a75780b0001040000000004e9030000', 'tail': '504b070851895a8f1316000078890100', 'central_extra': '5554050003b7c0a76a75780b0001040000000004e9030000'},
+    'data/inputs_keyboard.scr': {'flag': 9, 'vm': 798, 'exta': 2175008768, 'local_extra': '5554090003c34ea86ac34ea86a75780b0001040000000004e9030000', 'tail': '504b070858ba53cab3080000a63d0000', 'central_extra': '5554050003c34ea86a75780b0001040000000004e9030000'},
+    'data/inputs_def.scr': {'flag': 9, 'vm': 798, 'exta': 2175008768, 'local_extra': '55540900034121a86a4121a86a75780b0001040000000004e9030000', 'tail': '504b07080f1a60a60f1000000f4b0000', 'central_extra': '55540500034121a86a75780b0001040000000004e9030000'},
+}
